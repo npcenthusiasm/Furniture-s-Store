e08f50f3d8f5a6d765640a991bff172b6fa2d4e6
npcenthusiasm <npcenthusiasm@outlook.com>
2019-09-17 01:30:14 +0800

    signin/signout compelete

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  created() {
+    const api = `${process.env.API_PATH}/api/${process.env.CUSTOMPATH}/products`;
+    this.$http.get(api).then((response) => {
+      console.log(response.data);
+    });
+  },
+};
+</script>
+
+<style lang="scss">
+@import "./assets/all.scss";
+</style>
---
+++ src/main.js
@@ -0,0 +1,38 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import App from './App';
+import router from './router';
+
+Vue.config.productionTip = false;
+Vue.use(VueAxios, axios);
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>',
+});
+
+router.beforeEach((to, from, next) => {
+  console.log('to', to, 'from', from, 'next', next);
+  if (to.meta.requiresAuth) {
+    console.log('需要驗證');
+    const api = `${process.env.API_PATH}/api/user/check`;
+    axios.post(api).then((response) => {
+      console.log(response.data);
+      console.log('123');
+      if (response.data.success) {
+        next();
+      } else {
+        next({
+          path: '/login',
+        });
+      }
+    });
+  } else {
+    next();
+  }
+});
